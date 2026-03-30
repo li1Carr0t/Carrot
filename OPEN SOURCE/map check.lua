8ccc7c4e5c9f97316a43b1d8e48ea1aff6863999
840Hz <99117606+li1Carr0t@users.noreply.github.com>
2026-03-31 04:00:55 +0700

    Add files via upload

---
+++ OPEN SOURCE/map check.lua
@@ -0,0 +1,9 @@
+-- Made by 840Hz
+
+if game.PlaceId == 2753915549 then
+	print("Wow You Here")
+elseif game.PlaceId == 4442272183 then
+	print("yoo you in other map")
+else
+   game.Players.LocalPlayer:Kick("Reason Here / Mot Found Map")  -- Reason Here
+end
